c3685dfd23523a8aa79f946d2c26b24c78c2b9ad
BuildTools <unconfigured@null.spigotmc.org>
2019-09-22 23:41:20 -0400

    Added 3 tabs with their respective anchor panes, buttons, text fields, labels and tables. Also added H2 database.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    @FXML
+    private Button testBut;
+
+    @FXML
+    void addProduct(MouseEvent event) {
+        System.out.println("Hi");
+    }
+    @FXML
+    void recordProduct(MouseEvent event) {
+        System.out.println("Hi");
+    }
+
 }
