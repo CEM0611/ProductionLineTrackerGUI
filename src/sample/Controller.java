003220a079ca1caec9f7cf4f51a4b0c9fc5fca7d
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 23:16:23 -0400

    Added completed integration with database as well as documentation, comments where needed and moving code. Overall, completed Sprint 1 and alpha version of GUI Program.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,21 +1,146 @@
+/**
+ * Florida Gulf Coast University.
+ * COP 3003 Object Oriented Programming Course
+ *
+ * @author Cristian Mendoza
+ * This file contains the majority of the code used for database operation
+ * and storage as well as general program interaction.
+ */
+
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+
 public class Controller {
+  // setting global variables for use within methods
+  private Connection conn;
 
-    @FXML
-    private Button testBut;
+  @FXML
+  private Button testBut;
 
-    @FXML
-    void addProduct(MouseEvent event) {
-        System.out.println("Hi");
+  @FXML
+  private TextField productName;
+
+  @FXML
+  private TextField Manufacturer;
+
+  @FXML
+  private ChoiceBox<String> itemType;
+
+  @FXML
+  private TableView<?> existingProductTable;
+
+  @FXML
+  private TableColumn<?, ?> ManufacturerColumn;
+
+  @FXML
+  private TableColumn<?, ?> ProductsColumn;
+
+  @FXML
+  private TableColumn<?, ?> itemTypeColumn;
+
+  @FXML
+  private ComboBox<String> recQuantity;
+
+  @FXML
+  private Button recP;
+
+  @FXML
+  /*
+   * initialize() method is used to preset the values for
+   *  both the "Choose Quantity" combo-box in the
+   *  Production tab and the "Item Type" choice-box in the Product Line tab.
+   */
+  public void initialize() {
+    ObservableList<String> itemTypeOptions =
+        FXCollections.observableArrayList(
+            "Audio",
+            "Visual",
+            "AudioMobile",
+            "VisualMobile"
+        );
+    itemType.setItems(itemTypeOptions);
+    itemType.getSelectionModel().selectFirst();
+    ObservableList<String> recQuantityOptions =
+        FXCollections.observableArrayList(
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"
+        );
+    recQuantity.setItems(recQuantityOptions);
+    recQuantity.setEditable(true);
+    recQuantity.getSelectionModel().selectFirst();
+  }
+
+  /*
+   * addProduct() method is used to collect data entered from
+   * specified text fields and insert them into the
+   * database previously connected to by using the initializeDB() method.
+   */
+  @FXML
+  void addProduct(MouseEvent event) {
+    try {
+      initializeDB(); // Initializes database connection
+/*      PreparedStatement stmt;
+      stmt = conn.createStatement();*/
+      String pName = productName.getText();
+      String mName = Manufacturer.getText();
+      String tName = itemType.getSelectionModel().getSelectedItem();
+      String sq1 = "INSERT INTO PRODUCT (NAME,TYPE,MANUFACTURER) " +
+          "VALUES ('" + mName + "', '" + pName + "', '" + tName + "')";
+      PreparedStatement preparedStatement1 = conn.prepareStatement(sq1);
+      int result = preparedStatement1.executeUpdate();
+      preparedStatement1.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
-    @FXML
-    void recordProduct(MouseEvent event) {
-        System.out.println("Hi");
+
+  }
+
+  @FXML
+  void recordProduct(MouseEvent event) {
+    System.out.println("Sorry, this isn't implemented yet!");
+  }
+
+  /*
+   * initializeDB() is used to initialize the connection
+   *  to the database for interaction between the program and the database.
+   */
+  private void initializeDB() {
+    // properties, Username and password needed to access database
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/H2";
+    final String USER = "";
+    final String PASS = "";
+    try {
+      // connect to database
+      Class.forName(JDBC_DRIVER);
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
+  }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,3 +1,15 @@
+/**
+ * Florida Gulf Coast University
+ * COP 3003 Object Oriented Programming Course
+ *
+ * @author Cristian Mendoza
+ * This current project is the alpha version of the GUI Program
+ * made for recording and tracking a production line of
+ * various products of various type of various manufacturers.
+ * This java file in particular handles the starting
+ * properties and parameters of the program.
+ */
+
 package sample;
 
 import javafx.application.Application;
@@ -7,17 +19,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+  // Starting method that collects resources from the sample.fxml file first then loads window properties and execution
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("Production Line Tracker GUI");
+    primaryStage.setScene(new Scene(root, 600, 400));
+    primaryStage.show();
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
